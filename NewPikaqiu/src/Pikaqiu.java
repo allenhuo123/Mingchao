4a8e24885accdcf0ce971cfc16fdaa7a766b1320
AllenHuo <19huomingchao@bprep.org>
2018-09-06 09:03:56 -0700

    Pikaqiu

---
+++ NewPikaqiu/src/Pikaqiu.java
@@ -0,0 +1,8 @@
+public class Pikaqiu {
+    public static void main(String[] args){
+        System.out.println("(\\__/)");
+        System.out.println("(*^.^)");
+        System.out.print("(_(\")(\")");
+
+    }
+}
